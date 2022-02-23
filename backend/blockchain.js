e09bc546ca273a5470663f7c85a1d9ed91ac722b
Restu Indera <rstx32@gmail.com>
2022-02-23 20:58:43 +0700

    add blockchain validation

--- backend/blockchain.js
+++ backend/blockchain.js
@@ -1,4 +1,4 @@
-const CryptoJS = require('crypto-js')
+const SHA256 = require('crypto-js').SHA256
 
 class Block {
   // block mempunyai atribut index, previousHash, timestamp, data, dan hash
@@ -13,7 +13,14 @@ class Block {
 
 // fungsi untuk menghitung hash
 const calculateHash = (index, prevHash, timestamp, data) => {
-  return CryptoJS.SHA256(index + prevHash + timestamp + data).toString()
+  return SHA256(index + prevHash + timestamp + data).toString()
+}
+
+// fungsi untuk menghitung hash (parameter : block)
+const blockCalculateHash = (block) => {
+  return SHA256(
+    block.index + block.previousHash + block.timestamp + block.data
+  ).toString()
 }
 
 // fungsi untuk mendapatkan block terakhir
@@ -59,16 +66,89 @@ const newBlock = (data) => {
     data,
     calculateHash(newIndex, prevHash, newTimestamp, data)
   )
-  // masukan block baru ke dalam blockchain
-  blockchain.push(block)
+  addBlock(block)
+}
+
+// menambahkan block ke dalam blockchain
+const addBlock = (block) => {
+  if (isStructureValid(block) && isBlockValid(latestBlock(), block)) {
+    blockchain.push(block)
+  }
+}
+
+// memvalidasi isi tipe data block
+const isStructureValid = (block) => {
+  return (
+    typeof block.index === 'number' &&
+    typeof block.previousHash === 'string' &&
+    typeof block.timestamp === 'number' &&
+    typeof block.data === 'object' &&
+    typeof block.data.voter === 'string' &&
+    typeof block.data.vote === 'string' &&
+    typeof block.data.origin === 'string' &&
+    typeof block.hash === 'string'
+  )
+}
+
+// memvalidasi block baru
+// 1. cek struktur block
+// 2. cek apakah index block baru lebih besar dari block sebelumnya
+// 3. cek apakah newBlock.prevHash sama dengan hash block sebelumnya
+// 4. cek ulang hash block
+const isBlockValid = (prevBlock, newBlock) => {
+  if (!isStructureValid(newBlock)) {
+    console.log(`invalid block structure!`)
+    return false
+  } else if (prevBlock.index + 1 !== newBlock.index) {
+    console.log('invalid index block')
+    return false
+  } else if (newBlock.previousHash !== prevBlock.hash) {
+    console.log('invalid previous hash')
+    return false
+  } else if (blockCalculateHash(newBlock) !== newBlock.hash) {
+    console.log(`invalid block hash`)
+    return false
+  }
+  return true
+}
+
+// validasi blockchain, parameter : blockchain[]
+// cek genesis kemudian cek block satu per satu
+const isBlockchainValid = (blockchain) => {
+  const isGenesisValid = () => {
+    return SHA256(blockchain[0]).toString() === SHA256(genesis).toString()
+  }
+
+  if (!isGenesisValid()) {
+    console.log(`genesis invalid`)
+    return false
+  }
+
+  for (let i = 1; i < blockchain.length; i++) {
+    if (!isBlockValid(blockchain[i - 1], blockchain[i])) {
+      return false
+    }
+  }
+
+  return true
 }
 
 // coba input block
 newBlock({
-  voter: "contoh@gmail.com",
-  vote: "02",
-  origin: "192.168.1.187",
+  voter: 'contoh@gmail.com',
+  vote: '02',
+  origin: '192.168.1.187',
 })
+const prev = latestBlock()
+
+newBlock({
+  voter: 'contoh2@gmail.com',
+  vote: '01',
+  origin: '192.168.1.182',
+})
+const next = latestBlock()
+
+isBlockchainValid(blockchain)
 
 // export module
 module.exports = { getBlocks, newBlock }
