1f4bb5a59ae1ca48ca8654d8477d1cac6cf31a97
Restu Indera <rstx32@gmail.com>
2022-03-31 14:30:51 +0700

    add url api variable

--- backend/getAPI.js
+++ backend/getAPI.js
@@ -1,22 +1,25 @@
 import fetch from 'node-fetch'
+import dotenv from 'dotenv'
+dotenv.config({ path: './config/.env' })
+const apiUrl = process.env.API_URL
 
 const getCandidates = async (req, res) => {
-  const candidates = await fetch('http://localhost/getcandidates')
+  const candidates = await fetch(`http://${apiUrl}/getcandidates`)
   return await candidates.json()
 }
 
 const getVoterPasswd = async (id) => {
-  const voter = await fetch(`http://localhost/voter/passwd/${id}`)
+  const voter = await fetch(`http://${apiUrl}/voter/passwd/${id}`)
   return await voter.json()
 }
 
 const getVoterPubKey = async (id) => {
-  const voter = await fetch(`http://localhost/voter/pubkey/${id}`)
+  const voter = await fetch(`http://${apiUrl}/voter/pubkey/${id}`)
   return await voter.json()
 }
 
 const getVoter = async (id) => {
-  const voter = await fetch(`http://localhost/voter/${id}`)
+  const voter = await fetch(`http://${apiUrl}/voter/${id}`)
   return await voter.json()
 }
 
