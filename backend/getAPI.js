c6683f859829f265a45d19987ef7f1a02e2c0b2e
Restu Indera <rstx32@gmail.com>
2022-03-31 14:56:00 +0700

    remove var

--- backend/getAPI.js
+++ backend/getAPI.js
@@ -1,25 +1,24 @@
 import fetch from 'node-fetch'
 import dotenv from 'dotenv'
 dotenv.config({ path: './config/.env' })
-const apiUrl = process.env.API_URL
 
 const getCandidates = async (req, res) => {
-  const candidates = await fetch(`http://${apiUrl}/getcandidates`)
+  const candidates = await fetch(`http://${process.env.API_URL}/getcandidates`)
   return await candidates.json()
 }
 
 const getVoterPasswd = async (id) => {
-  const voter = await fetch(`http://${apiUrl}/voter/passwd/${id}`)
+  const voter = await fetch(`http://${process.env.API_URL}/voter/passwd/${id}`)
   return await voter.json()
 }
 
 const getVoterPubKey = async (id) => {
-  const voter = await fetch(`http://${apiUrl}/voter/pubkey/${id}`)
+  const voter = await fetch(`http://${process.env.API_URL}/voter/pubkey/${id}`)
   return await voter.json()
 }
 
 const getVoter = async (id) => {
-  const voter = await fetch(`http://${apiUrl}/voter/${id}`)
+  const voter = await fetch(`http://${process.env.API_URL}/voter/${id}`)
   return await voter.json()
 }
 
